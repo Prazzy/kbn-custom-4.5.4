326bca4d3ce37b905d688004dd38f0197fe610a4
Prazzy <prazzee@gmail.com>
2017-01-05 22:15:20 -0800

    Fix for frozen filter bar

--- src/ui/public/filter_bar/lib/mapBool.js
+++ src/ui/public/filter_bar/lib/mapBool.js
@@ -2,30 +2,28 @@ define(function (require) {
   var _ = require('lodash');
   return function mapBoolProvider(Promise) {
     return function (filter) {
-      if (!filter.bool.should) return Promise.reject(filter);
-        var sub_filters = filter.bool.should;
-        var key;
-        values = _.map(sub_filters, function(fl){
-          var field;
-          var action = '<%= value %>';
-          if (fl.query) {
-            if (fl.query.prefix) {
-              field = fl.query.prefix;
-              action = '<%= value %>*';
-            } else if (fl.query.wildcard) {
-              field = fl.query.wildcard;
-            } 
-          } else if (fl.bool) {
-            field = fl.bool.must.term;      
-          }   
-          key = Object.keys(field)[0];
+      if (!filter.bool) return Promise.reject(filter);
+      var subFilters = filter.bool.should;
+      var key;
+      var values = _.map(subFilters, function (fl) {
+        var field;
+        var action = '<%= value %>';
+        if (fl.query) {
+          if (fl.query.prefix) {
+            field = fl.query.prefix;
+            action = '<%= value %>*';
+          } else if (fl.query.wildcard) {
+            field = fl.query.wildcard;
+          }
+        } else if (fl.bool) {
+          field = fl.bool.must.term;
+        }
+        key = Object.keys(field)[0];
 
-          var bool_template = _.template(action);
-          return bool_template({'value': field[key]});
-        });
-        return Promise.resolve({ key: key, value: values.join() });
-      
-
+        var boolTemplate = _.template(action);
+        return boolTemplate({'value': field[key]});
+      });
+      return Promise.resolve({ key: key, value: values.join() });
     };
   };
 
