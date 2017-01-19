1c30d53d06c946c269703c9f5172e76840799369
Prazzy <prazzee@gmail.com>
2017-01-18 23:26:54 -0800

    Editable filter bar feature - support for range query

--- src/ui/public/filter_bar/lib/mapBool.js
+++ src/ui/public/filter_bar/lib/mapBool.js
@@ -1,6 +1,7 @@
 define(function (require) {
   var _ = require('lodash');
-  return function mapBoolProvider(Promise) {
+  var moment = require('moment');
+  return function mapBoolProvider(Promise, courier) {
     return function (filter) {
       if (!filter.bool) return Promise.reject(filter);
       var subFilters = filter.bool.should;
@@ -16,18 +17,60 @@ define(function (require) {
             field = fl.query.wildcard;
           } else if (fl.query.regexp) {
             field = fl.query.regexp;
+          } else if (fl.query.range) {
+            field = fl.query.range;
+            key = Object.keys(field)[0];
+            if (field[key].gte && field[key].lte) return field[key].gte + ' TO ' + field[key].lte;
+            else if (!field[key].lte) return '>=' + field[key].gte;
+            if (!field[key].gte) return '<=' + field[key].lte;
           } else {
             field = fl.query;
           }
-        } else if (fl.bool) {
+        } else if (fl.range) {
+          //field = Object.keys(fl.range)[0];
+          field = fl.range;
+        }
+        else if (fl.bool) {
           field = fl.bool.must.term;
         }
-        key = Object.keys(field)[0];
 
+        // if (filter.meta.index) {
+        //   var fieldValue;
+        //   return courier.indexPatterns.get(filter.meta.index).then(function (indexPattern) {
+        //       debugger;
+        //       key = Object.keys(field)[0];
+        //       let indexField = indexPattern.fields.byName[key];
+        //       let value = field[key];
+        //       if (indexField) field[key] = indexField.format.convert(value);
+        //       if (indexField.format.constructor.name === 'DateTime') field[key] = value.replace(' ', 'T');
+        //       return Promise.resolve({ key: key, value: field[key]});
+        //   });
+        // } else {
+        key = Object.keys(field)[0];
         var boolTemplate = _.template(action);
-        return boolTemplate({'value': field[key]});
+        let fieldValue = field[key];
+        // if (moment(fieldValue).isValid()) {
+        //   fieldValue = moment(fieldValue).format('YYYY-MM-DD HH:mm:ss');
+        // }
+        return boolTemplate({'value': fieldValue});
+        //}
       });
-      return Promise.resolve({ key: key, value: values.join() });
+
+      if (filter.meta.index && values.length === 1) {
+        var fieldValue;
+        return courier.indexPatterns.get(filter.meta.index).then(function (indexPattern) {
+          let indexField = indexPattern.fields.byName[key];
+          let value;
+          if (indexField) {
+            value = (indexField.format.constructor.id === 'date') ?  Number(values[0]) : values[0];
+            if (isNaN(value)) value = values[0];
+            else value = indexField.format.convert(value);
+          } else value = values[0];
+          return Promise.resolve({ key: key, value: value});
+        });
+      } else {
+        return Promise.resolve({ key: key, value: values.join()});
+      }
     };
   };
 
