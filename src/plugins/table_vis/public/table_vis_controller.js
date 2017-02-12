1c9ed0e63cd3fe7e9e903b7b3ac5b4e0a47200d2
Prazzy <prazzee@gmail.com>
2017-02-11 20:59:58 -0800

    Extended metric calculation for table vis

--- src/plugins/table_vis/public/table_vis_controller.js
+++ src/plugins/table_vis/public/table_vis_controller.js
@@ -1,4 +1,5 @@
 define(function (require) {
+  const _ = require('lodash');
   // get the kibana/table_vis module, and make sure that it requires the "kibana" module if it
   // didn't already
   const module = require('ui/modules').get('kibana/table_vis', ['kibana']);
@@ -30,9 +31,68 @@ define(function (require) {
 
       $scope.hasSomeRows = hasSomeRows;
       if (hasSomeRows) {
+        const params = $scope.vis.params;
+        // PAC Feature:  extended metric cols
+        if (params.formulaEnabled) {
+          let newRows = [];
+          let newCols = [];
+          let cols = tableGroups.tables[0].columns;
+          let rows = tableGroups.tables[0].rows;
+          let formulaCols = params.formulaCols.split(',');
+          let formula = params.formula;
+          let formulaLabel = params.formulaLabel;
+          let metricColAppended = 0;
+          _.forEach(cols, function (col) {
+            if (_.includes(formulaCols, col.title) && !metricColAppended) {
+              col.title = formulaLabel;
+              newCols.push(col);
+              metricColAppended = 1;
+            } else {
+              if (!_.includes(formulaCols, col.title)) newCols.push(col);
+            }
+          });
+          //newCols = cols.slice(0, 2);
+          tableGroups.tables[0].columns = newCols;
+          //delete tableGroups.tables[0].columns[2];
+
+          _.forEach(rows, function (row) {
+            let newRow = [];
+            let metrics = [];
+            let cachedCol = '';
+            _.forEach(row, function (col) {
+              if (col.type !== 'metric') {
+                newRow.push(col);
+              } else if (!_.includes(formulaCols, col.aggConfig.params.customLabel)) {
+                newRow.push(col);
+              } else {
+                cachedCol = col;
+                const metric = {
+                  label: col.aggConfig.params.customLabel,
+                  value: col.value
+                };
+                metrics.push(metric);
+                metrics[col.aggConfig.params.customLabel] = metric;
+              }
+            });
+            let computedValue;
+            try {
+              const func = Function('metrics', 'return ' + formula);
+              computedValue = func(metrics);
+            } catch (e) {
+              computedValue = '';
+            }
+            if (cachedCol) {
+              cachedCol.value = computedValue;
+              newRow.push(cachedCol);
+            }
+            newRows.push(newRow);
+          });
+          tableGroups.tables[0].rows = newRows;
+        }
+        // PAC Feature:  extended metric cols
+
         $scope.tableGroups = tableGroups;
       }
     });
   });
-
 });
